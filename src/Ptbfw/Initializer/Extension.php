62abf75af9ee2048948cf899aba1653c75da5987
angel koilov <angel.koilov@gmail.com>
2012-08-26 17:32:15 +0300

    project init

---
+++ src/Ptbfw/Initializer/Extension.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace \Ptbfw\Initializer;
+
+/**
+ * Ptbfw extension for project initialization
+ *
+ * @author Angel Koilov <angel.koilov@gmail.com>
+ */
+class Extension extends \Behat\Behat\Extension\Extension {
+	
+}
