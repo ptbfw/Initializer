63dc9c7e891580c6ffa8026e57c628ee36566d88
angel koilov <angel.koilov@gmail.com>
2012-09-26 00:02:56 +0300

    add memcached initer

---
+++ src/Ptbfw/Initializer/Initers/Memcached.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ptbfw\Initializer\Initers;
+
+/**
+ * Description of Memcached
+ *
+ * @author Angel Koilov <angel.koilov@gmail.com>
+ */
+class Memcached implements Init {
+
+	private $memchached;
+
+	public function __construct($options) {
+		$this->memchached = new \Memcached();
+		foreach ($options['servers'] as $server) {
+			if (isset($options['port'])) {
+				$port = $options['port'];
+			} else {
+				$port = 11211;
+			}
+
+			if (isset($options['host'])) {
+				$host = $options['host'];
+			} else {
+				$host = 'localhost';
+			}
+			$result = $this->memchached->addServer($host, $port);
+			if ($result == false) {
+				throw new \Exception("cant add memchache server");
+			}
+		}
+	}
+
+	public function reset() {
+		$result = $this->memchached->flush();
+		if ($result == false) {
+			throw new \Exception('memchache clear failed');
+		}
+	}
+
+}
